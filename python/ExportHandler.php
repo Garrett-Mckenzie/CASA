8c6e86028cd50fd2ed2187099ad7c3e3e2ec54fa
presetprogrammer <33395240+presetprogrammer@users.noreply.github.com>
2025-10-20 11:53:33 -0400

    Export updated

--- python/ExportHandler.php
+++ python/ExportHandler.php
@@ -2,8 +2,8 @@
     $python_script = 'CASA_DB_Calls.py';
     $output = [];
     $return_var = 0;
-    if($_SERVER['REQUEST_METHOD'] === 'post' && isset($_POST)){
-        /*
+    $temp = "";
+    if($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST)){
         $req = $_POST['export'];
         if($req === 'donor'){
             $query = "SELECT * FROM donors;";
@@ -11,30 +11,24 @@
         elseif($req === 'donations'){
             $query = "SELECT * FROM donations;";
         }
-        */
-        switch($_POST['export']){
-            case "donor":
-                $query = "SELECT * FROM donors;";
-                $export_file = __DIR__ . DIRECTORY_SEPARATOR . "exports" . DIRECTORY_SEPARATOR . "donors.xlsx";
-
-                break;
-            case "donation":
-                $query = "SELECT * FROM donations;";
-                $export_file = __DIR__ . DIRECTORY_SEPARATOR . "exports" . DIRECTORY_SEPARATOR . "donations.xlsx";
-                break;
-            default:
-                exit(1);
+        else{
+            header("Location: export.html?success=0");
         }
+        
         exec("python \"$python_script\" -e \"$query\" \"$export_file\"", $output, $return_var);
+        $temp = "exec successful";
         if($return_var === 0){
-            $download = "exports" . DIRECTORY_SEPARATOR . $_POST['export'] . "s.xlsx";
+            $download = "exports" . DIRECTORY_SEPARATOR . 'donorExport' . ".xlsx";
             header("Location: export.html?success=1&file=$download");
             exit;
         }
         else{
             header("Location: export.html?success=0");
-            echo "Export failed";
             exit;
         }
+    }
+    else{
+        header("Location: export.html?success=0&file=$temp");
+        exit;
     }
 ?>
