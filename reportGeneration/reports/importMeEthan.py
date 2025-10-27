07fa6b3eee979597b695935147c163e8e59ea91e
Ethan Bostick <ethan.bostick@gmail.com>
2025-10-27 11:33:11 -0400

    some data stuff

--- reportGeneration/reports/importMeEthan.py
+++ reportGeneration/reports/importMeEthan.py
@@ -25,7 +25,7 @@ import traceback
 import mariadb
 from datetime import datetime
 
-def connect():
+def winConnect():
     try:
         conn = mariadb.connect(
                 user = "casadb",
@@ -36,6 +36,22 @@ def connect():
         return conn
     except Exception as e:
         raise e
+    
+def macConnect():
+    try:
+        conn = mariadb.connect(
+            user="casadb",
+            password="casadb",
+            host="localhost",
+            database="casadb",
+            unix_socket="/Applications/XAMPP/xamppfiles/var/mysql/mysql.sock"
+        )
+        return conn
+    except Exception as e:
+        raise e
+
+
+
 
 def executeQuery(query,conn):
     try:
@@ -43,7 +59,7 @@ def executeQuery(query,conn):
         cur.execute(query)
         result = cur.fetchall()
         return result
-    except Excetion as e:
+    except Exception as e:
         raise e
 
 #icky solution but just role with it
@@ -115,11 +131,11 @@ class PDF:
         table.setStyle(table_style)
         self.elements.append(table)
         self.elements.append(Spacer(1,12))
-
-    def insertGraph(self,plot,width,height):
+    
+    #def insertGraph(self,plot,width,height):
         plt.savefig("./reports/plot.png") 
         self.elements.append(Image("reports/plot.png",width=width*inch,height=height*inch))
     
     #This method actually makes the pdf
     def buildPDF(self):
-        self.doc.build(self.elements) 
+        self.doc.build(self.elements)
--- reportGeneration/reports/makeReport.py
+++ reportGeneration/reports/makeReport.py
@@ -4,24 +4,19 @@ from importMeGarrett import *
 try:
     #get options as specified by the user
     args = getArgs()
-    
-    ### data stuff ###
-    #questions to answer with report:
-        #top x donors by total amount donated
-        #top x donors by avg amount donated
-        #boxplots for top 5 cities and the average donation amount
-        #same for zipcodes ^
 
-        #given an event, total donations toward event y over time x (this might be included on the specific event page)
-        #descriptive stats over sample dataset
-            
-        
+    ### data stuff ###
+   
 
     ### end data stuff ###
     
     #connect to database
+    #args['os'] = 'l'
     try:
-        conn = connect()
+        if args['os'] == 'w':
+            conn = winConnect()
+        else:
+            conn = macConnect()
     except Exception as e:
         raise e
 
@@ -62,6 +57,7 @@ try:
     #Insert a graph
     data = pd.DataFrame({"Height":[1,2,3,4,5,6,7,8,9,10],"Johnson":[2,4,6,8,10,12,14,16,18,20]})
     plot = sns.scatterplot(data=data,x="Height",y="Johnson")
+
     pdf.insertGraph(plot,5,5)
 
     #make the pdf (THIS MUST BE CALLED LAST)
