a4c968f964b77a1886baf8981f772df90a92be07
Ethan Bostick <ethan.bostick@gmail.com>
2025-11-16 15:59:50 -0500

    fixed header in specificEvent.php

--- specificEvent.php
+++ specificEvent.php
@@ -1,3 +1,29 @@
+<?php
+    // Template for new VMS pages. Base your new page on this one
+
+    // Make session information accessible, allowing us to associate
+    // data with the logged-in user.
+    session_cache_expire(30);
+    session_start();
+    date_default_timezone_set("America/New_York");
+
+    $loggedIn = false;
+    $accessLevel = 0;
+    $userID = null;
+    if (isset($_SESSION['_id'])) {
+        $loggedIn = true;
+        // 0 = not logged in, 1 = standard user, 2 = manager (Admin), 3 super admin (TBI)
+        $accessLevel = $_SESSION['access_level'];
+        $userID = $_SESSION['_id'];
+    }
+    if ($accessLevel < 1) {
+        header('Location: login.php');
+        die();
+    }
+
+?>
+
+
 <!DOCTYPE html>
 <html>
     <head>
