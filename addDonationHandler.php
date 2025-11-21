423ae150114071a62cacee158807de9c733cd5a1
Customer <u3214-ig86wwhdgupy@gvam1111.siteground.biz>
2025-11-21 17:23:33 +0000

    Moving to localhost because Im not evil

--- addDonationHandler.php
+++ addDonationHandler.php
@@ -1,5 +1,18 @@
 <?php
 
-require_once '../database/dbinfo.php';
+include_once '../database/dbinfo.php';
+
+var_dump($_POST);
+//Read in donation info
+$firstName = $_POST["firstName"];
+$lastName = $_POST["lastName"];
+$date = $_POST["date"];
+$amount = $_POST["amount"];
+$fee = $_POST["fee"];
+$eventName = $_POST["eventName"];
+$reason = $_POST["reason"];
+
+$con = connect();
+
 
 ?>
